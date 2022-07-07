5fcf6e311666d6062a00fd69ba34df8d53da0abf
oscar mugambi <oscarmugambi@gmail.com>
2022-07-07 16:55:56 +0300

    added optimistic updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import Mutation from './components/Mutation'
+import './App.css'
+import Delayed from './delayed/Delayed'
+import Optimistic from './optimistic/Optimistic'
+import Current from './current/Current'
 
 function App() {
   return (
     <div className='App'>
-      <Mutation />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Mutation />} />
+          <Route path='/delayed' element={<Delayed />} />
+          <Route path='/optimistic' element={<Optimistic />} />
+          <Route path='/current' element={<Current />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
